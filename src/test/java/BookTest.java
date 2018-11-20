4f329bace7a2ac783b28e895515ce51ceb2c13df
ionawright <ionawright@live.com>
2018-11-20 23:14:15 +0000

    book_tests_complete

---
+++ src/test/java/BookTest.java
@@ -0,0 +1,30 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
+public class BookTest {
+
+    Books book;
+
+    @Before
+    public void before() {
+        book = new Books("A streetcat called Bob", "James Bowen", "Autobiography");
+    }
+
+    @Test
+    public void hasTitle() {
+        assertEquals("A streetcat called Bob", book.getTitle());
+    }
+
+    @Test
+    public void hasAuthor(){
+    assertEquals("James Bowen", book.getAuthor());
+    }
+
+    @Test
+    public void hasGenre(){
+    assertEquals("Autobiography", book.getGenre());
+    }
+
+}
